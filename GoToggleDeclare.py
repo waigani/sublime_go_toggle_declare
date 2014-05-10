b2d91e97a04d7b96ee5a3d6ee79cebd72e77bd9f
waigani <waigani@gmail.com>
2014-05-10 17:59:03 +1200

    Basic functionality working

---
+++ GoToggleDeclare.py
@@ -0,0 +1,15 @@
+import sublime, sublime_plugin
+
+class GoToggleDeclareCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for region in self.view.sel():  
+            # Only interested in empty regions, otherwise they may span multiple  
+            # lines, which doesn't make sense for this command.  
+            if region.empty():  
+                line = self.view.line(region) 
+                lineContents = self.view.substr(line)
+                if ":=" in lineContents:
+                	lineContents = lineContents.replace(":=","=", 1)
+                else:
+                	lineContents = lineContents.replace("=", ":=", 1)
+                self.view.replace(edit, line, lineContents) 
